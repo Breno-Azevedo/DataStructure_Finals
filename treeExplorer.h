a8c0d7915119506268c3cb2fb493a82bed39125c
Sofia Lakschevitz <sofia.laks15@gmail.com>
2023-06-30 20:14:06 -0300

    Modificando o .h

--- treeExplorer.h
+++ treeExplorer.h
@@ -16,7 +16,7 @@
 
 #include <iostream>
 
-// Structs
+/*--------------------------Struct Declaration-------------------------------*/
 struct Node {
     /**
      * @brief Basic node structure for the tree.
@@ -30,30 +30,38 @@ struct Node {
  
 struct ListNode {
     /**
-     * @brief Basic node structure for the list.
-     * The lists are used to sort the tree.
+     * @brief Basic node structure for the linked list.
+     * The linked lists are used to sort the tree.
      */
     int iPayload;
     struct ListNode * ptrNext;
 };
 
 struct QueueNode {
-    Node* ptrNode;
-    QueueNode* ptrNext;
+    /**
+     * @brief Basic node structure for the queue.
+     * The queues are used in the Breadth First Search Algorithm.
+     */
+    struct Node * ptrNode;
+    struct QueueNode * ptrNext;
 };
 
 struct Queue {
-    QueueNode* ptrFront;
-    QueueNode* ptrRear;
+    /**
+     * @brief Structure for the queue.
+     * The queues are used in the Breadth First Search Algorithm.
+     */
+    struct QueueNode * ptrFront;
+    struct QueueNode * ptrRear;
 };
 
-// Functions headers for the treeExplorer.cpp
+/*-----------------Functions headers for the treeExplorer.cpp-----------------*/
 
 /**
  * @brief createNode() - Creates a new node with the given payload.
  * 
  * @param iPayload The payload to be stored in the node.
- * @return struct Node* A pointer to the newly created node.
+ * @return struct Node * A pointer to the newly created node.
  */
 struct Node * createNode(int iPayload);
 
@@ -61,45 +69,44 @@ struct Node * createNode(int iPayload);
  * @brief createListNode() - Creates a List Node object with the given payload.
  * 
  * @param iPayload
- * @return struct ListNode*
+ * @return struct ListNode *
  */
 struct ListNode* createListNode(int iPayload);
 
-
 /**
- * @brief insertNode() - Given an existing Node and its iPayload,
- * creates a new node and insert it into the existing one.
+ * @brief insertNode() - Given an existing Node (root) and a iPayload,
+ * creates a new node and insert it into the existing one (into the tree).
  * If the given Node is nullptr, than it creates a brand new one.
  * 
  * @param ptrRoot 
  * @param iPayload 
- * @return struct Node* 
+ * @return struct Node * 
 **/
 struct Node * insertNode(struct Node * ptrRoot, int iPayload);
 
 
 /**
- * @brief createTreeTxt() - Given a txt file, it creates a
+ * @brief createTreeTxt() - Given a .txt file, it creates a
  * BST with the elements of the txt file.
  * 
  * @param fileName 
- * @return struct Node* 
+ * @return struct Node * 
  */
 struct Node * createTreeTxt(const char * fileName);
 
 /**
  * @brief searchNode() - Given the root of a tree and an integer,
- * it search through the looking for Node that contains the respective
+ * it search through the tree looking for a Node that contains the respective
  * integer and returns it.
  * 
  * @param ptrRoot 
  * @param iPayload 
- * @return Node* 
+ * @return struct Node * 
 **/
 struct Node * searchNode(struct Node * ptrRoot, int iPayload);
 
 /**
- * @brief printTree() - Prints the elements of the tree
+ * @brief printTree() - Prints the elements of the tree.
  * 
  * @param ptrRoot 
  */
@@ -111,14 +118,14 @@ void printTree(struct Node * ptrRoot);
  * @param ptrQueue 
  * @return bool 
  */
-bool queueEmpty(Queue* ptrQueue);
+bool queueEmpty(struct Queue * ptrQueue);
 
 /**
  * @brief queuePop() - Removes the first element of the queue.
  * 
  * @param ptrQueue 
  */
-void queuePop(Queue* ptrQueue);
+void queuePop(struct Queue * ptrQueue);
 
 /**
  * @brief queuePush() - Inserts a new element into the queue.
@@ -126,18 +133,18 @@ void queuePop(Queue* ptrQueue);
  * @param ptrQueue 
  * @param ptrNode 
  */
-void queuePush(Queue* ptrQueue, Node* ptrNode);
+void queuePush(struct Queue* ptrQueue, struct Node* ptrNode);
 
 /**
  * @brief queueFront() - Returns the first element of the queue.
  * 
  * @param ptrQueue 
- * @return Node* 
+ * @return struct Node * 
  */
-Node* queueFront(Queue* ptrQueue);
+struct Node * queueFront(struct Queue * ptrQueue);
 
 /**
- * @brief Breadth_First_Search() - Executes the BFS algorithm
+ * @brief Breadth_First_Search() - Executes the BFS algorithm.
  * 
  * @param ptrRoot 
  */
@@ -149,7 +156,7 @@ void Breadth_First_Search(struct Node * ptrRoot);
  * 
  * @param ptrNode 
  * @param iPayload 
- * @return Node* 
+ * @return struct Node * 
  */
 struct Node * SearchElement(struct Node* ptrRoot, int iPayload);
 
@@ -171,55 +178,56 @@ int sizeTree(struct Node * ptrRoot);
 
 /**
  * @brief lesserNode() - Returns the lesser node of the tree.
+ * The element with the smaller iPayload.
  * 
  * @param ptrRoot 
- * @return struct Node* 
+ * @return struct Node * 
  */
-struct Node* lesserNode(struct Node* ptrRoot);
+struct Node * lesserNode(struct Node * ptrRoot);
 
 /**
  * @brief deleteNode() - Deletes a node from the tree by its payload.
- * Auxiliary function for the deleteNode().
  * 
  * @param ptrRoot 
  * @param iData 
- * @return struct Node* 
+ * @return struct Node * 
  */
-struct Node* deleteNode(struct Node* ptrRoot, int iData);
+struct Node * deleteNode(struct Node * ptrRoot, int iData);
 
 /**
- * @brief treeToList() - Given a tree, it creates a list with the elements
+ * @brief treeToList() - Given a tree, it creates a linked list with the elements
  * of the tree. This function is used in the sorting algorithms.
  * 
  * @param ptrStartingNode 
  * @param ptrHead 
- * @return struct ListNode* 
+ * @return struct ListNode * 
  */
 struct ListNode * treeToList(struct Node * ptrStartingNode, struct ListNode * ptrHead);
 
 /**
- * @brief printList() - Prints the elements of the list.
+ * @brief printList() - Prints the elements of the linked list.
  * 
  * @param ptrHead 
  */
-void printList(struct ListNode* ptrHead);
+void printList(struct ListNode * ptrHead);
 
 /**
- * @brief insertList() - Inserts a new node into the list.
+ * @brief insertList() - Inserts a new node into the linked list.
  * 
  * @param ptrHead 
  * @param iValue 
- * @return struct ListNode* 
+ * @return struct ListNode * 
  */
-struct ListNode * insertList(struct ListNode **ptrHead, int iValue);
+struct ListNode * insertList(struct ListNode ** ptrHead, int iValue);
 
 /**
- * @brief buildTree() - Builds a tree with the given size.
+ * @brief buildTree() - Builds a tree with the given size and
+ * inputs from the user.
  * 
  * @param iSize 
- * @return struct Node* 
+ * @return struct Node * 
  */
-struct Node* buildTree(int iSize);
+struct Node * buildTree(int iSize);
 
 /**
  * @brief completeTree() - Checks if the tree is complete.
@@ -230,7 +238,7 @@ struct Node* buildTree(int iSize);
  * @return true 
  * @return false 
  */
-bool completeTree(struct Node* ptrRoot);
+bool completeTree(struct Node * ptrRoot);
 
 /**
  * @brief fullLevel() - Checks if the given level is full.
@@ -242,7 +250,7 @@ bool completeTree(struct Node* ptrRoot);
  * @return true 
  * @return false 
  */
-bool fullLevel(struct Node* ptrRoot, int iLevel);
+bool fullLevel(struct Node * ptrRoot, int iLevel);
 
 /**
  * @brief perfectTree() - Checks if the tree is perfect.
@@ -252,33 +260,31 @@ bool fullLevel(struct Node* ptrRoot, int iLevel);
  * @return true 
  * @return false 
  */
-bool perfectTree(struct Node* ptrRoot);
+bool perfectTree(struct Node * ptrRoot);
 
 
-// Functions headers for the sortingAlgorithms.cpp
+/*---------------Functions headers for the sortingAlgorithms.cpp--------------------------*/
 
 /**
- * @brief selectionSort() - Sorts the list using the selection sort algorithm.
+ * @brief selectionSort() - Sorts the linked list using the selection sort algorithm.
  * 
  * @param ptrHead 
  */
-void selectionSort(struct ListNode** ptrHead);
+void selectionSort(struct ListNode ** ptrHead);
 
 /**
- * @brief insertionSort() - Sorts the list using the insertion sort algorithm.
+ * @brief insertionSort() - Sorts the linked list using the insertion sort algorithm.
  * 
  * @param ptrHead 
  */
-void insertionSort(struct ListNode** ptrHead);
+void insertionSort(struct ListNode ** ptrHead);
 
 /**
- * @brief bubbleSort() - Sorts the list using the bubble sort algorithm.
+ * @brief bubbleSort() - Sorts the linked list using the bubble sort algorithm.
  * 
  * @param head 
  */
-void bubbleSort(struct ListNode* head);
-
-
+void bubbleSort(struct ListNode * head);
 
 /**
  * @brief FindNodeByPos() - Given a list and a position, it returns the node
@@ -286,16 +292,16 @@ void bubbleSort(struct ListNode* head);
  * 
  * @param ptrHead 
  * @param position 
- * @return struct ListNode* 
+ * @return struct ListNode * 
  */
-struct ListNode* FindNodeByPos(struct ListNode* ptrHead, int position);
+struct ListNode * FindNodeByPos(struct ListNode * ptrHead, int position);
 
 /**
  * @brief ShellSort() - Sorts the list using the shell sort algorithm.
  * 
  * @param ptrHead 
  */
-void ShellSort(struct ListNode** ptrHead);
+void ShellSort(struct ListNode ** ptrHead);
 
 // Functions headers for the menu.cpp
 
@@ -304,7 +310,7 @@ void ShellSort(struct ListNode** ptrHead);
  * on the main function via CLI.
  * 
  * @param ptrRoot 
- * @return struct Node* 
+ * @return struct Node * 
  */
 struct Node * insertNodeMain(struct Node * ptrRoot);
 
@@ -312,7 +318,7 @@ struct Node * insertNodeMain(struct Node * ptrRoot);
  * @brief createTreeTxtMain() - Calls the createTreeTxt() function
  * on the main function via CLI.
  * 
- * @return struct Node* 
+ * @return struct Node * 
  */
 struct Node * createTreeTxtMain();
 
@@ -321,7 +327,7 @@ struct Node * createTreeTxtMain();
  * on the main function via CLI.
  * 
  * @param ptrRoot 
- * @return struct Node* 
+ * @return struct Node * 
  */
 struct Node * searchElementMain(struct Node * ptrRoot);
 
@@ -337,7 +343,7 @@ void selectionSortMain(struct Node * ptrRoot);
  * @brief buildTreeMain() - Calls the buildTree() function
  * on the main function via CLI.
  * 
- * @return struct Node* 
+ * @return struct Node * 
  */
 struct Node * buildTreeMain();
 
@@ -387,7 +393,7 @@ void sizeTreeMain(struct Node * ptrRoot);
 
 void heightTreeMain(struct Node * ptrRoot);
 
-//MENU ASCII
+/*----------------------MENU ASCII--------------------------------------------*/
 /*Cria a linha de cima*/
 void createUpperBound(int iSize);
 
@@ -413,5 +419,6 @@ void swap(ListNode* a, ListNode* b);
 
 #include "./treeExplorer.hpp"
 #include "./sortingAlgorithms.hpp"
+#include "./menu.hpp"
 
 #endif // TREEEXPLORER_H
